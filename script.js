0eed39af6b0cd1f856679905d9a4569083ea0994
Emowtion <emowtion@hotmail.com>
2020-08-30 19:33:51 +0200

    added mobile and line chart

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-var ctx = document.getElementById('myChart');
-var myChart = new Chart(ctx, {
+var trafficChart = document.getElementById('myChart').getContext('2d');
+
+var traffic = new Chart(trafficChart, {
     
     type: 'line',
     data: {
         responsive: true,
-        labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26','27-3','4-10','11-17','18-24','25-31'],
+        labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26','27-3','4-10'],
         datasets: [{
             label: '',
             data: [ 500, 
                 1000, 
                 1500, 
-                2000, 
+                2000,
                 2500, 
-                3000 ],
+                2500, 
+                3000,
+                500,
+                1500 ],
             backgroundColor: [
                 'rgba( 128, 86, 212 , 0.2)',
             ],
@@ -33,6 +37,82 @@ var myChart = new Chart(ctx, {
             yAxes: [{
                 ticks: {
                     min: 500,
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+const dailyChart = document.getElementById('daily-chart');
+const daily = new Chart(dailyChart, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+var mobileChart = document.getElementById('mobile-chart');
+var mobile = new Chart(mobileChart, {
+    type: 'doughnut',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
                     beginAtZero: true
                 }
             }]
